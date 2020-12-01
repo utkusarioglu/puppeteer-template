4e1d6152c5e0c770e5af0426f0d934b3b230d738
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-12-01 21:16:14 +0300

    Documentation: for delay module

--- src/delay.ts
+++ src/delay.ts
@@ -1,3 +1,7 @@
+/**
+ * Promisifies setTimeout to be used as a delay function
+ * @param time - time in miliseconds for delay
+ */
 export async function delay(time: number): Promise<void> {
   return new Promise(function (resolve) {
     setTimeout(resolve, time);
